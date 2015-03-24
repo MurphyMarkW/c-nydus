8936edbe020f07009eae27e4366acd511eec5924
MurphyMarkW <murphy.mark.w+github@gmail.com>
2015-03-24 02:23:41 -0500

    Added tcp2udt standalone code.

---
+++ src/nydus/tcp2udt.c
@@ -0,0 +1,31 @@
+#include <nydus/tcp.h>
+
+
+// TCP host and port.
+char * source_host = "localhost";
+char * source_port = "9000";
+
+// UDT host and port.
+char * target_host = "localhost";
+char * target_port = "9090";
+
+
+#define fmt(s) "[Nydus] " s
+int main(int argc, char **argv) {
+    // TODO use argp for argument parsing
+
+    // Tell syslog to print to stderr as well.
+    openlog(NULL, LOG_PERROR, LOG_USER);
+
+    int retval = nydus_tcp.proxy(
+        source_host,
+        source_port,
+        target_host,
+        target_port
+    );
+
+    closelog();
+
+    return 0;
+}
+#undef fmt//[Nydus]
